6b2f7dec6a6ab94afaf09cec28dd92b0f577ad1e
SandraBergstrom <bergstrom_sandra@hotmail.com>
2023-05-26 09:49:16 +0000

    Changing parents from children

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,17 +1,18 @@
 import logo from './logo.svg';
 import './App.css';
-import FunctionalGreetingWithProps from './components/FunctionalGreetingsWithProps'
-import StatfulGreetingWithPrevState from './components/StatefulGreetingWithPrevState';
-import EventsFunctional from './components/EventsFunctional';
-import EventsClass from './components/EventsClass';
-import ConditionalRenderingClass from './components/ConditionalRenderingClass';
-import ConditionalRenderingFunctional from './components/ConditionalRenderingFunctional';
-import NestingComponents from './components/NestingComponents';
+// import FunctionalGreetingWithProps from './components/FunctionalGreetingsWithProps'
+// import StatfulGreetingWithPrevState from './components/StatefulGreetingWithPrevState';
+// import EventsFunctional from './components/EventsFunctional';
+// import EventsClass from './components/EventsClass';
+// import ConditionalRenderingClass from './components/ConditionalRenderingClass';
+// import ConditionalRenderingFunctional from './components/ConditionalRenderingFunctional';
+// import NestingComponents from './components/NestingComponents';
+import MethodsAsPropsParent from './components/MethodsAsPropsParent';
 
 function App() {
   return (
     <div className="App">
-      <NestingComponents />
+      <MethodsAsPropsParent />
     </div>
   );
 }
