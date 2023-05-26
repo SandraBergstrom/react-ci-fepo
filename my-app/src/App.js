34fd526388360267095c3a86b0416a19bc6463f0
SandraBergstrom <bergstrom_sandra@hotmail.com>
2023-05-26 07:31:34 +0000

    Binding events

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -2,11 +2,14 @@ import logo from './logo.svg';
 import './App.css';
 import FunctionalGreetingWithProps from './components/FunctionalGreetingsWithProps'
 import StatfulGreetingWithPrevState from './components/StatefulGreetingWithPrevState';
+import EventsFunctional from './components/EventsFunctional';
+import EventsClass from './components/EventsClass';
 
 function App() {
   return (
     <div className="App">
-      <StatfulGreetingWithPrevState name="Sandra"/>
+      <EventsFunctional />
+      <EventsClass />
     </div>
   );
 }
