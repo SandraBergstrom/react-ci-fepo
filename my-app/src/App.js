6da0c2bd4baf3b06113a3651f133a577986c5adf
SandraBergstrom <bergstrom_sandra@hotmail.com>
2023-05-29 07:48:04 +0000

    Component did mount

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -9,11 +9,12 @@ import './App.css';
 // import NestingComponents from './components/NestingComponents';
 import MethodsAsPropsParent from './components/MethodsAsPropsParent';
 import RenderingLists from './components/RenderingLists';
+import LifeCyclesCDM from './components/LifeCyclesCDM';
 
 function App() {
   return (
     <div className="App">
-      <RenderingLists />
+      <LifeCyclesCDM />
     </div>
   );
 }
