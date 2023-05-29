1dd1e8811e1e185b925b63e2b7c811ded3868e07
SandraBergstrom <bergstrom_sandra@hotmail.com>
2023-05-29 13:24:21 +0000

    Searchbar

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -12,13 +12,14 @@ import './App.css';
 // import LifeCyclesCDM from './components/LifeCyclesCDM';
 // import LifeCyclesCDU from './components/LifeCyclesCDU';
 // import LifeCyclesCWU from './components/LifeCyclesCWU';
-import ControlledForm from './components/ControlledForm';
-import UncontrolledForm from './components/UncontrolledForm';
+// import ControlledForm from './components/ControlledForm';
+// import UncontrolledForm from './components/UncontrolledForm';
+import SearchBar from './components/SearchBar';
 
 function App() {
   return (
     <div className="App">
-      <UncontrolledForm />
+      <SearchBar />
     </div>
   );
 }
