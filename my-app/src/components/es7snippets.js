920ed0b23b7c4909c36214f918c820c1b3496bde
SandraBergstrom <bergstrom_sandra@hotmail.com>
2023-05-29 09:04:22 +0000

    es7snippets

---
+++ my-app/src/components/es7snippets.js
@@ -0,0 +1,59 @@
+// rfce - Creates a functional component
+// import React from 'react'
+
+function es7Snippets() {
+    return (
+      <div>es7Snippets</div>
+    )
+  }
+ 
+  export default es7Snippets
+ 
+ 
+ 
+ // rafce - creates an Arrow functional component
+  import React from 'react'
+ 
+  const es7Snippets = () => {
+    return (
+      <div>es7Snippets</div>
+    )
+  }
+ 
+  export default es7Snippets
+ 
+ 
+ 
+ // rce - creating a class component
+  import React, { Component } from 'react'
+ 
+  export class es7Snippets extends Component {
+    render() {
+      return (
+        <div>es7Snippets</div>
+      )
+    }
+  }
+ 
+  export default es7Snippets
+ 
+ 
+ 
+ // rconst - creating the constructor
+  constructor(props) {
+    super(props)
+ 
+    this.state = {
+       first
+    }
+  }
+ 
+ 
+ 
+ // clg - log to console
+  console.log(first)
+ 
+ 
+  
+ //clo - log an object along with its name as a string.
+ console.log('first', first)
