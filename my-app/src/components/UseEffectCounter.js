7ba04b4b43704c92ab662a6af69eba8a2ea7559e
SandraBergstrom <bergstrom_sandra@hotmail.com>
2023-05-31 07:22:40 +0000

    Effects only once

--- my-app/src/components/UseEffectCounter.js
+++ my-app/src/components/UseEffectCounter.js
@@ -1,18 +1,27 @@
-import React, { useState, useEffect } from 'react'
+import React, {useState, useEffect} from 'react'
 
 function UseEffectCounter() {
     const [count, setCount] = useState(0);
-    useEffect(() => {
-        document.title = count
-    });
+    const [time, setTime] = useState(0);
 
+    useEffect(() => {
+        console.log('count 1 effect');
+        document.title = count;
+    }, [count]);
+    useEffect(()=>{
+        console.log('Creating timer');
+        const interval = setInterval(() => {
+            setTime(time => time + 1)
+        }, 1000);
+    }, []);
     return (
         <div>
             <button onClick={
-                () => setCount(count => count +1)
+                () => setCount(count => count + 1)
             }>
-                Inrcrement Count ({count})
+                Increment Count ({count})
             </button>
+            <h2>Time is {time}</h2>
         </div>
     )
 }
