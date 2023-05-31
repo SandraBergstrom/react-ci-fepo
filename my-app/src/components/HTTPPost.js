00144b90d56af9324ecec9e3f211dcce4c2a14e3
SandraBergstrom <bergstrom_sandra@hotmail.com>
2023-05-31 11:45:06 +0000

    http hooks

--- my-app/src/components/HTTPPost.js
+++ my-app/src/components/HTTPPost.js
@@ -2,41 +2,41 @@ import React, { Component } from 'react'
 import axios from 'axios'
 
 export class HTTPPost extends Component {
-    constructor(props) {
-      super(props)
-    
-      this.state = {
-         apiResponse: null
+  constructor(props) {
+    super(props)
+
+    this.state = {
+      apiResponse: null
+    }
+  }
+  postToApi = () => {
+    axios.post(
+      'https://jsonplaceholder.typicode.com/posts',
+      {
+        title: 'Hello world',
+        body: 'It works',
+        userId: 123,
       }
-    }
-    postToApi = () => {
-        axios.post(
-            'https://jsonplaceholder.typicode.com/posts', 
-            {
-                title: 'Hello world',
-                body: 'It works',
-                userId: 123,
-            }
-        ).then(response => {
-            this.setState({
-                apiResponse: response.data
-            })            
-        })
-    }
+    ).then(response => {
+      this.setState({
+        apiResponse: response.data
+      })
+    })
+  }
   render() {
-    const {apiResponse} = this.state;
+    const { apiResponse } = this.state;
     return (
       <div>
         <button onClick={this.postToApi}>
-            Create Post
+          Create Post
         </button>
         {
-            apiResponse
+          apiResponse
             ? (<div>
-                <h1>{apiResponse.title}</h1>
-                <p>post id: {apiResponse.id}</p>
-                <p>{apiResponse.body}</p>
-                <p>user id: {apiResponse.userId}</p>
+              <h1>{apiResponse.title}</h1>
+              <p>post id: {apiResponse.id}</p>
+              <p>{apiResponse.body}</p>
+              <p>user id: {apiResponse.userId}</p>
             </div>)
             : (<p>please click the button above</p>)
         }
